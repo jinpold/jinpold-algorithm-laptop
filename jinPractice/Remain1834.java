6fb75daddfc511f9f55416f14dea8427722beea6
jinpold <jinpold@gmail.com>
2024-03-24 13:51:49 +0900

    240324 Remain

---
+++ jinPractice/Remain1834.java
@@ -0,0 +1,14 @@
+package jinPractice;
+import java.util.Scanner;
+public class Remain1834 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        long num = scan.nextLong();
+        long sum = 0;
+
+        for (int i = 0; i < num; i++) {
+            sum += (num + 1) * i;
+        }
+        System.out.println(sum);
+    }
+}
