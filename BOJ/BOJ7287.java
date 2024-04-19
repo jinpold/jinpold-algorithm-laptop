8ff776392f75a3faa5fc01cff8675179c4b4fe4e
jinpold <jinpold@gmail.com>
2024-04-20 07:41:12 +0900

    240420 BOJ

---
+++ BOJ/BOJ7287.java
@@ -0,0 +1,11 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class BOJ7287 {
+    public static void main(String[] args) {
+        System.out.println(42);
+        System.out.println("heojinmm1");
+
+    }
+}
