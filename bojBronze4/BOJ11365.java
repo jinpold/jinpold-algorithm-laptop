8320dcba2a41cf4172874d3b9a0e2e2f9ddb12d4
jinpold <jinpold@gmail.com>
2024-05-15 10:31:46 +0900

    240515 BOJ

--- bojBronze4/BOJ11365.java
+++ bojBronze4/BOJ11365.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class BOJ11365 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        while ((true)) {
+        while (true) {
             String text = sc.nextLine();
             if (text.equals("END")) break;
             char[] textArr = text.toCharArray();
