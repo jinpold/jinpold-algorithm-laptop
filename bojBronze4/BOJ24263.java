abdf2e98069dccba5b8bc26c14347d0ebaee3ed1
jinpold <jinpold@gmail.com>
2024-05-18 09:31:04 +0900

    240518 boj

---
+++ bojBronze4/BOJ24263.java
@@ -0,0 +1,12 @@
+package bojBronze4;
+
+import java.util.Scanner;
+public class BOJ24263 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println(N);
+        System.out.println(1);
+
+    }
+}
