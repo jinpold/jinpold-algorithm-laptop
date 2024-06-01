7c7657da03dc8ff0fd1dc0959a6ce52a1d8548bb
jinpold <jinpold@gmail.com>
2024-06-01 09:05:49 +0900

    240601 BOJ

---
+++ bojBronze4/BOJ16486.java
@@ -0,0 +1,13 @@
+package bojBronze4;
+
+import java.util.Scanner;
+
+public class BOJ16486 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int d1 = sc.nextInt();
+        int d2 = sc.nextInt();
+        double pie = 3.141592;
+        System.out.println( d1*2 + 2*pie*d2);
+    }
+}
