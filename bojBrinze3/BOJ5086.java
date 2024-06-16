94efde3ed56f288342584cd17117a9783b3b7d86
jinpold <jinpold@gmail.com>
2024-06-16 17:23:33 +0900

    240616 BOJ

---
+++ bojBrinze3/BOJ5086.java
@@ -0,0 +1,18 @@
+package bojBrinze3;
+
+import java.util.Scanner;
+
+public class BOJ5086 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true){
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            if (A == 0 && B == 0) break;
+            if (B % A == 0) System.out.println("factor");
+            else if (A % B == 0) System.out.println("multiple");
+            else System.out.println("neither");
+        }
+    }
+}
