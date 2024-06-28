d33a69cf4e63a1f28242c07693e9becee4ef300b
jinpold <jinpold@gmail.com>
2024-06-29 07:56:07 +0900

    240629 BOJ

---
+++ bojBrinze3/BOJ2490.java
@@ -0,0 +1,34 @@
+package bojBrinze3;
+
+import java.util.Scanner;
+
+public class BOJ2490 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < 3; i++) {
+            int cnt = 0;
+            for (int j = 0; j < 4; j++) {
+                int temp = sc.nextInt();
+                if(temp == 0) cnt++;
+            }
+            switch (cnt) {
+                case 0:
+                    System.out.println("E");
+                    break;
+                case 1:
+                    System.out.println("A");
+                    break;
+                case 2:
+                    System.out.println("B");
+                    break;
+                case 3:
+                    System.out.println("C");
+                    break;
+                case 4:
+                    System.out.println("D");
+                    break;
+            }
+        }
+
+    }
+}
