8bb9920630ecfefa9a389867f5dd9732b62c3c55
jinpold <jinpold@gmail.com>
2024-07-26 10:38:22 +0900

    240726 BOJ

---
+++ jinPractice/NumArr.java
@@ -0,0 +1,16 @@
+package jinPractice;
+
+public class NumArr {
+    public static void main(String[] args) {
+        int[] numArr = new int[5];
+        int result = 0;
+
+        for(int i=0; i<5; i++)
+            numArr[i] = ++i;
+
+        for(int i : numArr)
+            result += i;
+
+        System.out.printf("%d", result);
+    }
+}
