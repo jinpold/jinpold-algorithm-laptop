0c0b19d1ad30cc29d64c492c82f4150599ac78a3
jinpold <jinpold@gmail.com>
2024-08-17 14:51:55 +0900

    240817

---
+++ bojBronze2/BOJ1009.java
@@ -0,0 +1,24 @@
+package bojBronze2;
+
+import java.util.Scanner;
+
+public class BOJ1009 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int i = 0; i < T; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int result = 1;
+            for (int j = 0; j < b; j++) {
+                result = (result * a) % 10;
+            }
+            if (result == 0) {
+                System.out.println(10);
+            } else {
+                System.out.println(result);
+            }
+        }
+        sc.close();
+    }
+}
