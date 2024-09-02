b0c3b9522dbda47dbbe3d79d75d7fc31e569ce3e
jinpold <jinpold@gmail.com>
2024-09-02 11:12:11 +0900

    240902

---
+++ bojBronze2/BOJ1440.java
@@ -0,0 +1,41 @@
+package bojBronze2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ1440 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        StringBuilder str = new StringBuilder();
+
+        String time = br.readLine();
+
+        st = new StringTokenizer(time, ":");
+
+        int[] arr = new int[3];
+
+        arr[0] = Integer.parseInt(st.nextToken());
+        arr[1] = Integer.parseInt(st.nextToken());
+        arr[2] = Integer.parseInt(st.nextToken());
+
+        int cnt = 0;
+
+        for(int h = 0; h < 3; h++){
+            for(int m = 0; m < 3; m++) {
+                for (int s = 0; s < 3; s++) {
+                    if(h != m && m != s && s != h)
+                        if(arr[h] >= 1 && arr[h] <= 12 && arr[m] >= 0 && arr[m] <= 59 && arr[s] >= 0 && arr[s] <= 59)
+                            cnt++;
+                }
+            }
+        }
+
+        str.append(cnt);
+
+        System.out.print(str);
+        br.close();
+    }
+}
