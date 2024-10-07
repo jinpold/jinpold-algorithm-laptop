946258036e52dfd7f6787f96d2dc9d469658e1ba
jinpold <jinpold@gmail.com>
2024-10-07 18:13:54 +0900

    241007 BOJ

---
+++ bojBronze2/BOJ2948.java
@@ -0,0 +1,17 @@
+package bojBronze2;
+
+import java.util.Scanner;
+
+public class BOJ2948 {
+    public static void main(String[] args) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        int d = sc.nextInt(), m=sc.nextInt(), ans=0;
+        int [] days = new int[] {0,0,31,28,31,30,31,30,31,31,30,31,30,31};
+        String [] day = { "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "Monday", "Tuesday"};
+        for(int i=0;i<=m;i++)
+            ans+=days[i];
+        ans+=d;
+        System.out.println(day[ans%7]);
+    }
+
+}
