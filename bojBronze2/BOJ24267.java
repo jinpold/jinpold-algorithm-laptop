74129d743b3fd4b3745050c7fcca3ba1abbfd10e
jinpold <jinpold@gmail.com>
2024-10-25 13:01:20 +0900

    241025 BOJ

---
+++ bojBronze2/BOJ24267.java
@@ -0,0 +1,13 @@
+package bojBronze2;
+
+import java.util.Scanner;
+
+public class BOJ24267 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextInt();
+        System.out.println((a*(a-1)*(a-2))/6);
+        System.out.println(3);
+    }
+}
+
