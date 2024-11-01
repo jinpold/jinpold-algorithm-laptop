1b1a6a49ed7d2070958a8084f21e68cffcd66925
jinpold <jinpold@gmail.com>
2024-11-02 08:53:06 +0900

    241102 BOJ

--- bojBronze1/BOJ1037.java
+++ bojBronze1/BOJ1037.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class BOJ1037 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt(); // 약수의 개수
+        int N = sc.nextInt();
         int[] num = new int[N];
 
         for(int i=0; i<N; i++){
