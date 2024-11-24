c31a7db19cfe81e73c727d2ff94e37d80f88f116
jinpold <jinpold@gmail.com>
2024-11-24 20:51:21 +0900

    241124 BOJ

---
+++ bojBronze1/BOJ6996.java
@@ -0,0 +1,29 @@
+package bojBronze1;
+
+import java.util.*;
+
+public class BOJ6996 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        String ch1,ch2;
+        for(int i=0;i<n;i++){
+            ch1 = scanner.next();
+            ch2 = scanner.next();
+
+            char[] A = ch1.toCharArray();
+            char[] B = ch2.toCharArray();
+
+            Arrays.sort(A);
+            Arrays.sort(B);
+
+            if(Arrays.equals(A,B)){
+                System.out.println(ch1+ " & " +ch2+ " are anagrams.");
+            }else{
+                System.out.println(ch1+ " & " +ch2+ " are NOT anagrams.");
+            }
+        }
+    }
+}
