bc4157372a49354af35ec1a154cbebe4c82d0373
jinpold <jinpold@gmail.com>
2024-11-26 08:39:46 +0900

    241126 BOJ

---
+++ bojBronze1/BOJ13322.java
@@ -0,0 +1,15 @@
+package bojBronze1;
+
+
+import java.util.Scanner;
+
+public class BOJ13322 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        for (int i = 0; i < input.length(); i++) {
+            System.out.println(i);
+        }
+    }
+}
