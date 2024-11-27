c2f6a2510ce9bcdc4b8e863d31c11979bff96e52
jinpold <jinpold@gmail.com>
2024-11-28 08:34:24 +0900

    241128 BOJ

---
+++ bojBronze1/BOJ21965.java
@@ -0,0 +1,41 @@
+package bojBronze1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class BOJ21965 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        boolean b = true;
+
+        int size = Integer.parseInt(br.readLine());
+        int[] num = new int[size];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < size; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int count = 0;
+        for (int i = 1; i < size; i++) {
+            if (num[i - 1] < num[i]) {
+                if (count == -1) {
+                    b = false;
+                    break;
+                }
+                count++;
+            } else if (num[i - 1] == num[i]) {
+                b = false;
+                break;
+            } else {
+                count = -1;
+            }
+        }
+
+        System.out.print(b ? "YES" : "NO");
+    }
+}
